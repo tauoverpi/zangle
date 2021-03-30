88c4f69831597498aab89572fe736fb84959ef6f
Tau <tauoverpi@yandex.com>
2021-03-31 00:33:33 +0200

    Add pandoc step and support files

--- build.zig
+++ build.zig
@@ -1,5 +1,14 @@
 const std = @import("std");
+const Builder = std.build.Builder;
+const RunStep = std.build.RunStep;
 const lib = @import("lib/lib.zig");
+
+const pkgs = struct {
+    const args = std.build.Pkg{
+        .name = "args",
+        .path = "extern/zig-args/args.zig",
+    };
+};
 
 pub fn build(b: *std.build.Builder) !void {
     // Standard target options allows the person running `zig build` to choose
@@ -18,12 +27,23 @@ pub fn build(b: *std.build.Builder) !void {
     const tangle_step = b.step("tangle", "Extract executable code from documentation");
     tangle_step.dependOn(&tangler.step);
 
+    const fmt_step = b.addFmt(&.{"src"});
+    fmt_step.step.dependOn(tangle_step);
+
     const exe = b.addExecutable("zangle", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
-    exe.step.dependOn(tangle_step);
+    exe.step.dependOn(&fmt_step.step);
+    exe.addPackage(pkgs.args);
     exe.addLibPath("lib/lib.zig");
     exe.install();
+
+    const pdf_step = b.step("pdf", "Render documentation to PDF");
+    pdf_step.dependOn(&(try pandoc(b, &.{
+        "README.md",
+        "-o",
+        "out/manual.pdf",
+    })).step);
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
@@ -36,4 +56,23 @@ pub fn build(b: *std.build.Builder) !void {
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&b.addTest("lib/lib.zig").step);
+}
+
+fn pandoc(b: *Builder, args: []const []const u8) !*RunStep {
+    var list = std.ArrayList([]const u8).init(b.allocator);
+    try list.appendSlice(&[_][]const u8{
+        "pandoc",
+        "--pdf-engine=xelatex",
+        "--standalone",
+        "--filter=pandoc-crossref",
+        "--filter=pandoc-citeproc",
+        "--syntax-definition=misc/syntax.xml",
+        "--highlight-style=misc/syntax.theme",
+        "--csl=misc/ieee.csl",
+    });
+
+    try list.appendSlice(args);
+
+    const step = b.addSystemCommand(list.items);
+    return step;
 }
